90a0f3a900493166eb642dbd8e146fe59ef283b2
AmanUllah <M.Amanullah0830@gmail.com>
2024-12-20 13:00:37 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Suspense } from "react";
 import { Inter } from "next/font/google";
 import LoadingScreen from './components/LoadingScreen';
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <LoadingScreen />
+        <Suspense>
+          <LoadingScreen />
+        </Suspense>
         {children}
       </body>
     </html>
